c52ff28483f92179e2a91380f14ca29eff988922
BimxyzDev <bimaputra436123@gmail.com>
2025-12-11 07:59:24 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://peterodatcly.bimxyz.my.id";
-export const API_KEY = "ptla_abj2PXPKPRX92vPiAp4Du9HLPco2EcFBJOpsd0HxVjB";
+export const PANEL_URL = "https://domianpanel.com";
+export const API_KEY = "apikey_panel";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
