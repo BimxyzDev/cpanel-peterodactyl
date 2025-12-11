c3c03a38d274230c242432148975dea941845dcd
BimxyzDev <bimaputra436123@gmail.com>
2025-12-11 08:01:06 +0700

    Update panel configuration settings

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
 export const PANEL_URL = "https://domianpanel.com";
-export const API_KEY = "apikey_panel";
+export const API_KEY = "apikey_panel111";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
