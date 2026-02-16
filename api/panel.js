630009f412b1be280af11728886a9604c5846338
BimxyzDev <bimaputra436123@gmail.com>
2026-02-16 14:25:59 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -3,4 +3,4 @@ export const API_KEY = "apikey_panel111";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
-export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
+export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_24";
